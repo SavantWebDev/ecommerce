798fb11dacbedfec54b209bc25f51d3c2332bfd4
Gustavoloppes-dev <gustavoloppes.dev@gmail.com>
2023-11-17 22:32:50 -0300

    component de input criado

---
+++ src/app/auth/cadastro/page.js
@@ -0,0 +1,11 @@
+
+import Formulario from '@/app/Components/Formulario'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-[#797777]'><Formulario/></div>
+  )
+}
+
+export default page
