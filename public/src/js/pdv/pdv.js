a4a409e912d80a07b0e51258c43c2599538b6939
Lucas <pereirapro123@gmail.com>
2023-11-20 22:23:29 -0300

    Commit

--- public/src/js/pdv/pdv.js
+++ public/src/js/pdv/pdv.js
@@ -1,63 +1,41 @@
-
 var contadorCards = 0;
 var totalPreco = 0;
 
-function gerarCard() {
-    var codigo = document.getElementById("codigo").value;
-    var quantidade = parseInt(document.getElementById("quantidade").value);
-    var precoUnitario = document.getElementById("preco").value;
-    
-    // Validar se o preço unitário é um número antes de continuar
-    if (isNaN(parseFloat(precoUnitario))) {
-        alert("Por favor, insira um preço unitário válido.");
-        document.getElementById("codigo").value = "";
+function gerarCard(nome, ean, preco) { // depois adiciono quantidade
+  //var codigo = document.getElementById("codigo").value;
 
-        
-        return;
-    }
+  // Validar se o código é não vazio
+  if (ean.trim() === '') {
+    alert("Por favor, insira um código válido.");
+    return;
+  }
 
-    // Validar se a quantidade é um número inteiro maior que zero
-    if (isNaN(quantidade) || quantidade <= 0 || !Number.isInteger(quantidade)) {
-        alert("Por favor, insira uma quantidade válida.");
-        return;
-    }
+  contadorCards++;
+  var novoCard = document.createElement("div");
+  novoCard.id = "card" + contadorCards;
+  novoCard.innerHTML =
+    "<div class='produto-single-store'>" +
+    "<br>" +
+    "<span id='nomeE' class='nome-produto-compra' >" + nome + "</span>" +
+    "<span id='codigoE'>Código: " + ean + "</span>" +
+    "<div class='dados-compra-single'>" +
+    "<span id='quantidadeE'>Quantidade: " + "XX" + "</span>" +
+    "<span id='precoE'>Valor Total: R$" + preco + "</span>" + // Aqui multiplico o preço pela quantidade
+    "<div class='btn-comp-al'>" +
+    "<button class='btn-excluir' onclick='excluirCard(" + contadorCards + ")'>Excluir</button>" +
+    "</div>" +
+    "</div>" +
+    "</div>";
 
-    var precoTotal = quantidade * parseFloat(precoUnitario);
+  document.getElementById("cardsContainer").appendChild(novoCard);
+  document.getElementById("codigo").value = "";
 
-    totalPreco += precoTotal; // Adiciona o preço total ao total geral
-
-    contadorCards++;
-    var novoCard = document.createElement("div");
-    novoCard.id = "card" + contadorCards;
-    novoCard.innerHTML = 
-       "<div class='produto-single-store'>" +
-          "<br>" +
-          "<span id='codigoE' class='nome-produto-compra' > Código:" + codigo +"</span>" +
-          "<div class='dados-compra-single'>" +
-            "<span id='quantidadeE'>Quantidade: x"+ quantidade +"</span>" +
-            "<span id='precoE'>Valor Total: R$" + precoTotal.toFixed(2) + "</span>" +
-            "<div class='btn-comp-al'>" +
-              "<!--" +
-              "<button class='btn-editar' onclick='abrirModalEditar()'>Editar</button>" +
-              "-->" +
-              "<button class='btn-excluir' onclick='excluirCard(" + contadorCards + ")'>Excluir</button>" +
-            "</div>"
-          "</div>" +
-        "</div>"
-
-    document.getElementById("cardsContainer").appendChild(novoCard);
-    document.getElementById("codigo").value = "";
-    document.getElementById("preco").value = "";
-    document.getElementById("quantidade").value = 1; // Reinicia a quantidade para 1
-
-    atualizarTotal(); // Chama a função para atualizar o total na tela
-    //salvarCardsNoLocalStorage()
+  atualizarTotal(); // Chama a função para atualizar o total na tela
+  //salvarCardsNoLocalStorage()
 }
 
-
-
 function atualizarTotal() {
-    document.getElementById("totalPreco").innerHTML = "Total: R$" + totalPreco.toFixed(2);
+  document.getElementById("totalPreco").innerHTML = "Total: R$" + totalPreco.toFixed(2);
 }
 
 function excluirCard(cardID) {
@@ -111,47 +89,47 @@ function cancelarCompra() {
 }
 
 //Soma Preços
-document.getElementById("somaPrecos").innerHTML = 
-                                                  '<div class="info-total-descontos">' +
-                                                    '<span id="totalPreco" class="total-produtos">Total: R$0.00</span>' +
-                                                    '<span>Descontos:</span>' +
-                                                    '<span></span>' +
-                                                  '</div>';
-document.getElementById("precoTot").innerHTML =     
-                                            '<div class="modal-c">' +
-                                              '<div>' +
-                                              '<label for="codigo">Preço Total:</label>' +
-                                              '</div>' +
-                                              '<div>' +
-                                              '<label> R$ ---,--</label>' +
-                                              '</div>' +
-                                            '</div>';
-document.getElementById("descontoTot").innerHTML =     
-                                            '<div class="modal-c">' +
-                                              '<label for="preco">Desconto:</label>' +
-                                              '<label> ---,-- %</label>' +
-                                            '</div>';
-document.getElementById("precoFin").innerHTML =                 
-                                            '<div class="modal-c">' +
-                                            '<label for="quantidade">Preço Final:</label>' +
-                                            '<label> R$ ---,--</label>' +
-                                            '</div>';
-document.getElementById("formaPag").innerHTML =     
-                                              '<div class="modal-c">' +            
-                                                '<label for="formPag">Escolha uma forma de pagamento:</label>' +
-                                                '<select id="formpagmt" name="formas"  style="border-width: 1px; border-radius: 5px;">' +
-                                                    '<option value="especie">Dinheiro Especie</option>' +
-                                                    '<option value="cartao">Cartão</option>' +
-                                                    '<option value="boleto">Boleto</option>' +
-                                                '</select>' +
-                                              '</div>';
-document.getElementById("parcelas").innerHTML =      
-                                              '<div class="modal-c">' +           
-                                                '<label for="parcPag">Quantidade de Parcelas:</label>' +
-                                                '<select id="parcPagt" name="parcelas" style="border-width: 1px; border-radius: 5px;">' +
-                                                    '<option value="umP">1x</option>' +
-                                                    '<option value="doisP">2x</option>' +
-                                                    '<option value="tresP">3x</option>' +
-                                                    '<option value="quatroP">4x</option>' +
-                                                '</select>' +
-                                              '</div>';
+document.getElementById("somaPrecos").innerHTML =
+  '<div class="info-total-descontos">' +
+  '<span id="totalPreco" class="total-produtos">Total: R$0.00</span>' +
+  '<span>Descontos:</span>' +
+  '<span></span>' +
+  '</div>';
+document.getElementById("precoTot").innerHTML =
+  '<div class="modal-c">' +
+  '<div>' +
+  '<label for="codigo">Preço Total:</label>' +
+  '</div>' +
+  '<div>' +
+  '<label> R$ ---,--</label>' +
+  '</div>' +
+  '</div>';
+document.getElementById("descontoTot").innerHTML =
+  '<div class="modal-c">' +
+  '<label for="preco">Desconto:</label>' +
+  '<label> ---,-- %</label>' +
+  '</div>';
+document.getElementById("precoFin").innerHTML =
+  '<div class="modal-c">' +
+  '<label for="quantidade">Preço Final:</label>' +
+  '<label> R$ ---,--</label>' +
+  '</div>';
+document.getElementById("formaPag").innerHTML =
+  '<div class="modal-c">' +
+  '<label for="formPag">Escolha uma forma de pagamento:</label>' +
+  '<select id="formpagmt" name="formas"  style="border-width: 1px; border-radius: 5px;">' +
+  '<option value="especie">Dinheiro Especie</option>' +
+  '<option value="cartao">Cartão</option>' +
+  '<option value="boleto">Boleto</option>' +
+  '</select>' +
+  '</div>';
+document.getElementById("parcelas").innerHTML =
+  '<div class="modal-c">' +
+  '<label for="parcPag">Quantidade de Parcelas:</label>' +
+  '<select id="parcPagt" name="parcelas" style="border-width: 1px; border-radius: 5px;">' +
+  '<option value="umP">1x</option>' +
+  '<option value="doisP">2x</option>' +
+  '<option value="tresP">3x</option>' +
+  '<option value="quatroP">4x</option>' +
+  '</select>' +
+  '</div>';
--- public/src/js/pdv/pdvVerificador.js
+++ public/src/js/pdv/pdvVerificador.js
@@ -1,102 +1,60 @@
-// Checar se o imput foi clikado
-/*
+// Validador do código ean passado
 function checarTinput() {
-    var ElementoI = document.getElementById("codigo");
-    var ElementoP = document.getElementById("preco");
-    var ElementoQ = document.getElementById("quantidade");
+    var codigo = document.getElementById("codigo");
+    const Icodigo = codigo.value;
 
-    const inputValue = ElementoI.value;
-    const inputPreco = ElementoP.value;
-    const inputQuantidade = ElementoQ.value;
-    var tamanhoCaracteres = 15;
+    var tamanhoCaracteres = 13;
 
-    // Verificador de Regras de Passagem de Dados
-    // Verifica se inputValue existe e não está vazio
-    if (inputValue.length >= tamanhoCaracteres) {
-        ElementoI.value = inputValue.substring(0, tamanhoCaracteres);
+    // Verificador de tamanho
+    if (Icodigo.length >= tamanhoCaracteres) {
+        codigo.value = Icodigo.substring(0, tamanhoCaracteres);
 
-        // Verificador de Existência // Não pode ser um número que não exista e nehum vazio
-        if (inputValue && inputValue.trim() !== '') {
-            fetch(`https://api-n56x.onrender.com/v1/api/produto?ean=${inputValue}`)
-                .then(response => response.json())
-                .then(data => {
-                    if (data.ean) {
-                        gerarCard();
-                        document.getElementById("vizualizaProd").innerHTML =
-                            '<div id="displayValues" class="al-f-dados"></div>' +
-                            '<div class="vizualizar-dados">' +
-                            '<div class="al-f-dados">' +
-                            '<h1 class="nome-ad-prod font-nome"><b>Produto: Bohemia Lata</b></h1>' +
-                            '<h2 class="nome-ad-prod"><b>Código: ' + inputValue + '</b></h2>' +
-                            '<h2 class="nome-ad-prod"><b>Quantidade: x' + inputQuantidade + '</b></h2>' +
-                            '<h2 class="nome-ad-prod"><b>Valor : R$ ' + inputPreco + '</b></h2>' +
-                            '</div>' +
-                            '</div>';
-                        console.log(data.ean);
-                    } else {
-                        // Produto não encontrado, exibe um alerta
-                        alert('Produto não encontrado na API.');
-                    }
-                })
-                .catch(error => console.error('Erro:', error));
-        } else {
-            // Valor de inputValue é inválido ou vazio, exibe um alerta
-            alert('Por favor, insira um valor válido no input.');
-        }
+        verificarExApi(Icodigo)//, Ipreco, Iquantidade
+
     } else {
-        if (inputValue.length === 15) {
-            alert('Teste')
-        }
-    }
-
-}
-*/
-function checarTinput() {
-    var ElementoI = document.getElementById("codigo");
-    var ElementoP = document.getElementById("preco");
-    var ElementoQ = document.getElementById("quantidade");
-
-    const inputValue = ElementoI.value;
-    const inputPreco = ElementoP.value;
-    const inputQuantidade = ElementoQ.value;
-    var tamanhoCaracteres = 15;
-
-    // Verificador de Regras de Passagem de Dados
-    // Verifica se inputValue existe e não está vazio
-    if (inputValue.length >= tamanhoCaracteres) {
-        ElementoI.value = inputValue.substring(0, tamanhoCaracteres);
-
-        // Verificador de Existência // Não pode ser um número que não exista e nehum vazio
-        if (inputValue && inputValue.trim() !== '') {
-            fetch(`https://api-n56x.onrender.com/v1/api/produto?ean=${inputValue}`)
-                .then(response => response.json())
-                .then(data => {
-                    if (data.ean) {
-                        gerarCard();
-                        document.getElementById("vizualizaProd").innerHTML =
-                            '<div id="displayValues" class="al-f-dados"></div>' +
-                            '<div class="vizualizar-dados">' +
-                            '<div class="al-f-dados">' +
-                            '<h1 class="nome-ad-prod font-nome"><b>Produto: Bohemia Lata</b></h1>' +
-                            '<h2 class="nome-ad-prod"><b>Código: ' + inputValue + '</b></h2>' +
-                            '<h2 class="nome-ad-prod"><b>Quantidade: x' + inputQuantidade + '</b></h2>' +
-                            '<h2 class="nome-ad-prod"><b>Valor : R$ ' + inputPreco + '</b></h2>' +
-                            '</div>' +
-                            '</div>';
-                        console.log(data.ean);
-                    } else {
-                        // Produto não encontrado, exibe um alerta personalizado
-                        alert('Produto com código ' + inputValue + ' não encontrado na API.');
-                    }
-                })
-                .catch(error => console.error('Erro:', error));
-        } else {
-            // Valor de inputValue é inválido ou vazio, exibe um alerta
-            alert('Por favor, insira um valor válido no input.');
-        }
-    } else {
-        if (inputValue.length === 15) {
+        if (Icodigo.length >= 13) {
             alert('Teste')
         }
     }
 }
+
+// Verificador de Existência // Não pode ser um número que não exista e nenhum vazio
+function verificarExApi(Icodigo) {//, Ipreco, Iquantidade
+    if (Icodigo && Icodigo.trim() !== '') {
+        fetch(`https://api-n56x.onrender.com/v1/api/produto?ean=${Icodigo}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+
+                if (Icodigo.value != data.ean) {
+                    // Gera o card de vendas geral
+                    gerarCard(data.nomeproduto, data.ean, data.valor);
+
+                    // Gera o vizualizador do último produto passado
+                    document.getElementById("vizualizaProd").innerHTML =
+                        '<div id="displayValues" class="al-f-dados"></div>' +
+                        '<div class="vizualizar-dados">' +
+                        '<div class="al-f-dados">' +
+                        '<h1 class="nome-ad-prod font-nome"><b>Produto: ' + data.nomeproduto + '</b></h1>' +
+                        '<h2 class="nome-ad-prod"><b>Código: ' + data.ean + '</b></h2>' +
+                        '<h2 class="nome-ad-prod"><b>Preço: ' + data.valor + '</b></h2>' +
+                        '<h2 class="nome-ad-prod"><b>Quantidade: ' + 'XX' + '</b></h2>' +
+                        '<br>' +
+                        '<br>' +
+                        '<h2 class="nome-ad-prod"><b>Descrição: ' + 'data.descricao' + '</b></h2>' +
+                        '</div>' +
+                        '</div>';
+                } else {
+                    // Produto não encontrado, exibe um alerta personalizado
+                    // alert('Produto com código ' + Icodigo + ' não encontrado na API.');
+                    console.log('tttttttttttttttttt')
+                    alert('Teste');
+                }
+            })
+            .catch(error => console.error('Erro:', error));
+    } else {
+        // Valor de Icodigo é inválido ou vazio, exibe um alerta
+        alert('Por favor, insira um valor válido no input.');
+    }
+}
+
