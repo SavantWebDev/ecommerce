35b122d9d3a31de2c61ccb7f193285592d6f1a38
Lucas <pereirapro123@gmail.com>
2024-01-08 19:21:42 -0300

    Update pdv.js

--- public/src/js/pdv/pdv.js
+++ public/src/js/pdv/pdv.js
@@ -352,7 +352,8 @@ function emitirNota() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          //'Authorization': 'BearereyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkxvcHBlc0B0ZXN0ZS5jb20iLCJ1c2VybmFtZSI6IkxvcHBlcyIsImlhdCI6MTcwMjk5OTQxOSwiZXhwIjoxNzAzMDg1ODE5fQ.NZlOCZTcTyGFsteYN5aDSnh_H3VIt0d73fWYH64OM50',
+          //
+          'Authorization': 'BearereyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkxvcHBlc0B0ZXN0ZS5jb20iLCJ1c2VybmFtZSI6IkxvcHBlcyIsImlhdCI6MTcwMjk5OTQxOSwiZXhwIjoxNzAzMDg1ODE5fQ.NZlOCZTcTyGFsteYN5aDSnh_H3VIt0d73fWYH64OM50',
         },
         body: JSON.stringify(dados),
         
