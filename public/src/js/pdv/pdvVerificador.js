f23506674e7af02ba704fb03a04551471a845cde
Lucas <pereirapro123@gmail.com>
2024-02-29 20:58:46 -0300

    Commit

--- public/src/js/pdv/pdvVerificador.js
+++ public/src/js/pdv/pdvVerificador.js
@@ -32,7 +32,7 @@ function vizualizarImg(Urlimagem) {
 // Verificador de Existência // Não pode ser um número que não exista e nenhum vazio
 function verificarExApi(quantidadeA, codigoA) {//, Ipreco, Iquantidade
     if (codigoA && codigoA.trim() !== '') {
-        fetch(`https://api-n56x.onrender.com/v1/api/produtos/${codigoA}`)
+        fetch(`https://iceberg.savantweb.com.br/v1/api/produtos/${codigoA}`)
             .then(response => response.json())
             .then(data => {
 
